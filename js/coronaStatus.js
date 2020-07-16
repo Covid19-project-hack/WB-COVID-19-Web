d3c16be61fdf0d390a17d76cb50aa2fd2121a0ed
Sandykum <ssannthoshkumar@gmail.com>
2020-07-16 21:55:21 +0530

    master

--- js/coronaStatus.js
+++ js/coronaStatus.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     type: "GET",
     url: "https://api.covid19india.org/state_district_wise.json",
     success: function (returnedData) {
-      const district_data = returnedData["Manipur"].districtData;
+      const district_data = returnedData["Gujarat"].districtData;
       populateData(district_data);
     },
   });
